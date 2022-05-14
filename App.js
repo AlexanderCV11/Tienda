6d3488c83b9821f9e2ddbe7dc5ed1079ead15cad
Alex <alexander_corral_valdez@outlook.com>
2022-05-13 19:19:11 -0500

    commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './HomeScreen';
+import DetailScreen from './DetailScreen';
+import MenuScreen from './MenuScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="Tasks" component={MenuScreen} options={{title: "Tienda"}} />
+        <Stack.Screen name="Details" component={DetailScreen} options={{title: "Producto"}} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ data/tasks.js
@@ -0,0 +1,10 @@
+const tasks = [
+{stock: 300, id: 1, description: "Piña", url: "https://dibujafacil.com/vbz39s/una-pina-kawaii-3.jpg", nombre: "Piña dulce"},
+{stock: 250, id: 2, description: "Platano", url: "https://i.pinimg.com/564x/c6/9f/9c/c69f9c8691553aef02ad655705731273.jpg", nombre: "El unico fruto del amor"},
+{stock: 50, id: 3, description: "Papaya", url: "https://thumbs.dreamstime.com/z/icono-de-la-fruta-de-la-papaya-de-la-rebanada-del-kawaii-88015771.jpg", nombre:"Hay Papnatla tus hijos vuelan"},
+{stock: 25, id: 4, description: "Aguacate", url: "https://img.freepik.com/vector-gratis/personajes-dibujos-animados-lindo-aguacate-kawaii_537358-65.jpg?w=740", nombre: "wey es un aguacate kwaii"},
+{stock: 151, id: 5, description: "Sandia", url: "https://comodibujar.club/wp-content/uploads/2020/08/sandia-kawaii.jpg", nombre: "Esta bien sabrosa"},
+
+]
+
+export default tasks
